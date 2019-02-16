6c3261c3f978b02de5c287c8ccc6a6be70a655e1
Roker2 <dimaminko2000@gmail.com>
2019-02-16 10:47:35 +0000

    Try to fix BackupTool

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,9 +1,9 @@
 # LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
+    ro.omega.version=$(OMEGA_VERSION) \
+    ro.omega.releasetype=$(OMEGA_BUILD_TYPE) \
     ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
+    ro.modversion=$(OMEGA_VERSION) \
     ro.lineagelegal.url=https://lineageos.org/legal
 
 # LineageOS Platform Display Version
